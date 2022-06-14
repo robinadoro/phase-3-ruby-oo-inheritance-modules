b94c968912806308e9538050aed449e789c639a4
Robinson Idagiza <robinadoro@gmail.com>
2022-06-14 05:12:01 +0300

    commit tree bea1f561915fa9b3e0119066ae9fa78599d78341

---
+++ lib/meta_dancing_module.rb
@@ -0,0 +1,12 @@
+require_relative "../lib/kid.rb"
+require_relative "../lib/dancer.rb"
+
+
+
+module MetaDancing
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
+  
